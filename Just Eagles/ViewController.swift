87bc969f682746d144aa96b83b07cb9d36603975
Loo Yen Pin <looyenpin764@gmail.com>
2021-01-27 00:20:26 -0800

    Initial Commit

---
+++ Just Eagles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Just Eagles
+//
+//  Created by Yen Pin on 27/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Just EaglesTests/Just_EaglesTests.swift
@@ -0,0 +1,33 @@
+//
+//  Just_EaglesTests.swift
+//  Just EaglesTests
+//
+//  Created by Yen Pin on 27/1/21.
+//
+
+import XCTest
+@testable import Just_Eagles
+
+class Just_EaglesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
